1f6b5d9c378efd571e375492e14028625e594216
OutOfPlane <julius@peanutpay.de>
2023-10-22 01:51:15 +0200

    ADD: basic webserver config

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,14 @@
 #include "KMPCommon.h"
 #include "ArduinoJson.h"
 #include "esp_adc_cal.h"
+#include "website.h"
 
 #include <WiFi.h>
 #include <WiFiClient.h>
+#include <DNSServer.h>
+#include <WebServer.h>
+
+#define FW_VERS "1.0.0"
 
 #define SSID_NAME "PeanutPay"
 #define SSID_PASSWORD "PeanutPay"
@@ -18,19 +23,33 @@ const char GREEN[] = "#90EE90"; // LightGreen
 const char RED[] = "#FF4500";   // OrangeRed
 const char GRAY[] = "#808080";
 
+const char *serverStateStr = "keine Verbindung";
+
 char deviceID[13];
+uint8_t wifiMac[8];
 
 #define PIN_ADC0 34
 #define PIN_ADC1 35
 #define PIN_ADC2 39
 #define PIN_ADC3 36
 
+IPAddress apIP(8, 8, 8, 8);
+IPAddress netMsk(255, 255, 255, 0);
+
+// DNS server
+const byte DNS_PORT = 53;
+DNSServer dnsServer;
+WebServer webServer;
+
+char sensorDataBuff[1024] = {0};
+
+void updateSensorData();
 
 uint32_t readPinCal(uint8_t pin)
 {
   int adcResult = analogRead(pin);
   esp_adc_cal_characteristics_t adc_chars;
-  
+
   esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
   return esp_adc_cal_raw_to_voltage(adcResult, &adc_chars);
 }
@@ -86,6 +105,336 @@ bool requestServer(Client *netClient, const char *server, const char *path, cons
   return false;
 }
 
+/** Is this an IP? */
+boolean isIp(String str)
+{
+  for (int i = 0; i < str.length(); i++)
+  {
+    int c = str.charAt(i);
+    if (c != '.' && (c < '0' || c > '9'))
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+/** IP to String? */
+String toStringIp(IPAddress ip)
+{
+  String res = "";
+  for (int i = 0; i < 3; i++)
+  {
+    res += String((ip >> (8 * i)) & 0xFF) + ".";
+  }
+  res += String(((ip >> 8 * 3)) & 0xFF);
+  return res;
+}
+
+boolean captivePortal()
+{
+  if (!isIp(webServer.hostHeader()))
+  {
+    Serial.println("Request redirected to captive portal");
+    webServer.sendHeader("Location", String("http://") + toStringIp(webServer.client().localIP()), true);
+    webServer.send(302, "text/plain", "");
+    webServer.client().stop();
+    return true;
+  }
+  return false;
+}
+
+char pageBuff[1024];
+
+
+void applyConfig()
+{
+  if(webServer.arg("type") == "wifi")
+  {
+    Serial.println("Wifi Config!");
+    Serial.printf("SSID: %s\r\n", webServer.arg("s").c_str());
+    Serial.printf("PWD: %s\r\n", webServer.arg("m").c_str());
+    WiFi.begin(webServer.arg("s"), webServer.arg("m"));
+  }
+
+  if(webServer.arg("type") == "eth")
+  {
+    Serial.println("Ethernet DHCP Config!");
+    Ethernet.begin(wifiMac, 10000);
+  }
+
+  if(webServer.arg("type") == "ethman")
+  {
+    Serial.println("Ethernet manual Config!");
+    IPAddress ip, gateway, dns, netmask;
+
+    if(!ip.fromString(webServer.arg("ip")))
+    {
+      webServer.sendContent("<h2>IP Adresse fehlerhaft</h2>");
+      return;
+    }
+
+    if(!gateway.fromString(webServer.arg("ip")))
+    {
+      webServer.sendContent("<h2>Gateway Adresse fehlerhaft</h2>");
+      return;
+    }
+
+    if(!dns.fromString(webServer.arg("ip")))
+    {
+      webServer.sendContent("<h2>DNS Adresse fehlerhaft</h2>");
+      return;
+    }
+
+    if(!netmask.fromString(webServer.arg("ip")))
+    {
+      webServer.sendContent("<h2>Netmask fehlerhaft</h2>");
+      return;
+    }
+    
+    Ethernet.begin(wifiMac, ip, dns, gateway, netmask);
+  }
+  
+}
+
+void handleRoot()
+{
+  if (captivePortal())
+  {
+    return;
+  }
+  
+
+  webServer.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  webServer.sendHeader("Pragma", "no-cache");
+  webServer.sendHeader("Expires", "-1");
+  webServer.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  webServer.send(200, "text/html", "");
+
+
+  webServer.sendContent(webpageHeader1);
+  webServer.sendContent("Steuerbox Config");
+  webServer.sendContent(webpageHeader2);
+
+  sprintf(pageBuff, R"EOF(
+<nav>
+<b>Steuerbox</b>
+%s
+</nav>
+<div>
+<h1>Konfiguration</h1>
+<button onclick="window.location.href='/config';">
+Netzwerkeinstellungen
+</button>)EOF",
+          deviceID);
+  webServer.sendContent(pageBuff);
+
+  if(webServer.method() == HTTPMethod::HTTP_POST)
+  {
+    applyConfig();
+  }
+
+
+  webServer.sendContent(webpageDataTable);
+  webServer.sendContent(webpageTableUpdateScript);
+  webServer.sendContent("</div>");
+  webServer.sendContent(webpageFooter);
+}
+
+void handleConfig()
+{
+  if (captivePortal())
+  {
+    return;
+  }
+  webServer.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  webServer.sendHeader("Pragma", "no-cache");
+  webServer.sendHeader("Expires", "-1");
+  webServer.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  webServer.send(200, "text/html", "");
+
+
+  webServer.sendContent(webpageHeader1);
+  webServer.sendContent("Steuerbox Config");
+  webServer.sendContent(webpageHeader2);
+
+  sprintf(pageBuff, R"EOF(
+<nav>
+<b>Netzwerkeinstellungen</b>
+%s
+</nav>
+<div>
+<h1>Config</h1>
+<button onclick="window.location.href='/config_wifi';">
+WiFi
+</button><br>
+<button onclick="window.location.href='/config_eth_dhcp';">
+Ethernet DHCP
+</button><br>
+<button onclick="window.location.href='/config_eth_man';">
+Ethernet manuell
+</button><br>
+<button onclick="window.location.href='/';">
+&lt-Zurueck
+</button><br>
+)EOF",
+          deviceID);
+  webServer.sendContent(pageBuff);
+  webServer.sendContent("</div>");
+  webServer.sendContent(webpageFooter);
+}
+
+void handleConfigWiFi()
+{
+  if (captivePortal())
+  {
+    return;
+  }
+  webServer.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  webServer.sendHeader("Pragma", "no-cache");
+  webServer.sendHeader("Expires", "-1");
+  webServer.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  webServer.send(200, "text/html", "");
+
+
+  webServer.sendContent(webpageHeader1);
+  webServer.sendContent("Steuerbox Config");
+  webServer.sendContent(webpageHeader2);
+
+  sprintf(pageBuff, R"EOF(
+<nav>
+<b>Netzwerkeinstellungen</b>
+%s
+</nav>
+<div>
+<h1>WiFi</h1>
+<form action=/ method=post>
+<input type="hidden" name="type" value="wifi">
+<label>WiFi SSID:</label><input type=text name=s></input>
+<label>WiFi PWD:</label><input type=password name=m></input>
+<input type=submit value=Verbinden>
+</form><br><br>
+<button onclick="window.location.href='/config';">
+&lt-Zurueck
+</button><br>
+)EOF",
+          deviceID);
+  webServer.sendContent(pageBuff);
+  webServer.sendContent("</div>");
+  webServer.sendContent(webpageFooter);
+}
+
+void handleConfigEthDHCP()
+{
+  if (captivePortal())
+  {
+    return;
+  }
+  webServer.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  webServer.sendHeader("Pragma", "no-cache");
+  webServer.sendHeader("Expires", "-1");
+  webServer.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  webServer.send(200, "text/html", "");
+
+
+  webServer.sendContent(webpageHeader1);
+  webServer.sendContent("Steuerbox Config");
+  webServer.sendContent(webpageHeader2);
+
+  sprintf(pageBuff, R"EOF(
+<nav>
+<b>Netzwerkeinstellungen</b>
+%s
+</nav>
+<div>
+<h1>Ethernet DHCP</h1>
+<form action=/ method=post>
+<input type="hidden" name="type" value="eth">
+<input type=submit value=Verbinden>
+</form><br><br>
+<button onclick="window.location.href='/config';">
+&lt-Zurueck
+</button><br>
+)EOF",
+          deviceID);
+  webServer.sendContent(pageBuff);
+  webServer.sendContent("</div>");
+  webServer.sendContent(webpageFooter);
+}
+
+void handleConfigEthMan()
+{
+  if (captivePortal())
+  {
+    return;
+  }
+  webServer.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  webServer.sendHeader("Pragma", "no-cache");
+  webServer.sendHeader("Expires", "-1");
+  webServer.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  webServer.send(200, "text/html", "");
+
+
+  webServer.sendContent(webpageHeader1);
+  webServer.sendContent("Steuerbox Config");
+  webServer.sendContent(webpageHeader2);
+
+  sprintf(pageBuff, R"EOF(
+<nav>
+<b>Netzwerkeinstellungen</b>
+%s
+</nav>
+<div>
+<h1>WiFi</h1>
+<form action=/ method=post>
+<input type="hidden" name="type" value="ethman">
+<label>IP:</label><input type=text name=ip></input>
+<label>Gateway:</label><input type=text name=gw></input>
+<label>DNS:</label><input type=text name=dns></input>
+<label>Netmask:</label><input type=text name=nm></input>
+<input type=submit value=Verbinden>
+</form><br><br>
+<button onclick="window.location.href='/config';">
+&lt-Zurueck
+</button><br>
+)EOF",
+          deviceID);
+  webServer.sendContent(pageBuff);
+  webServer.sendContent("</div>");
+  webServer.sendContent(webpageFooter);
+}
+
+void dataRequestHandler()
+{
+  if (captivePortal())
+  {
+    return;
+  }
+  webServer.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  webServer.sendHeader("Pragma", "no-cache");
+  webServer.sendHeader("Expires", "-1");
+  webServer.send(200, "application/json", sensorDataBuff);
+}
+
+void handleNotFound()
+{
+  if (captivePortal())
+  {
+    return;
+  }
+  webServer.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  webServer.sendHeader("Pragma", "no-cache");
+  webServer.sendHeader("Expires", "-1");
+
+  String p;
+  p += F(
+      "<html><head></head><body>"
+      "<h1>Not found!!</h1>");
+  p += F("</body></html>");
+
+  webServer.send(404, "text/html", p);
+}
+
 void setup()
 {
   delay(5000);
@@ -122,6 +471,23 @@ void setup()
 
   Serial.printf("Hallo, Ich bin: %s\r\n", deviceID);
 
+  WiFi.softAPConfig(apIP, apIP, netMsk);
+  WiFi.softAP(("VV_SB_" + String(deviceID)).c_str(), "visioverdis");
+
+  dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
+  dnsServer.start(DNS_PORT, "*", apIP);
+
+  webServer.on("/", handleRoot);
+  webServer.on("/config", handleConfig);
+  webServer.on("/config_wifi", handleConfigWiFi);
+  webServer.on("/config_eth_dhcp", handleConfigEthDHCP);
+  webServer.on("/config_eth_man", handleConfigEthMan);
+  webServer.on("/data", dataRequestHandler);
+  webServer.on("/generate_204", handleRoot);
+  webServer.onNotFound(handleNotFound);
+  webServer.begin(); // Web server start
+  Serial.println("HTTP server started");
+
   IPAddress ipa(100, 100, 100, 5);
   IPAddress dns(100, 100, 100, 1);
   IPAddress gat(100, 100, 100, 1);
@@ -129,12 +495,9 @@ void setup()
 
   // Start the Ethernet connection and the server.
   // Ethernet.begin(wifiMac, ipa, dns, gat, nma);
-  if (Ethernet.begin(wifiMac) == 0)
+  if (Ethernet.begin(wifiMac, 10000) == 0)
   {
     Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    while (1)
-      ;
   }
 
   Serial.println("Ethernet IP:");
@@ -147,12 +510,14 @@ bool connOnce = false;
 uint32_t pollMillis = 0;
 uint32_t telemetryMillis = 0;
 uint32_t actionMillis = 0;
+uint32_t updateSensorMillis = 0;
 char respBuff[1000];
 char reqBuff[1000];
 
 Client *currentClient = &wifiClient;
 
-typedef enum{
+typedef enum
+{
   ACT_IDLE,
   ACT_PREPARED,
   ACT_STARTING,
@@ -164,8 +529,6 @@ bool pendingAction = false;
 int actionID = -1;
 int action = -1;
 actionState cActionState;
-
-
 
 void loop()
 {
@@ -181,12 +544,12 @@ void loop()
       if (deserializeJson(myDoc, respBuff) == DeserializationError::Ok)
       {
         Serial.println("Valid JSON!");
-        if(myDoc.containsKey("actionid"))
+        if (myDoc.containsKey("actionid"))
         {
-          //valid response
+          // valid response
           actionID = myDoc["actionid"];
           action = myDoc["action"];
-          if(actionID != -1)
+          if (actionID != -1)
           {
             pendingAction = true;
             Serial.println("Pending Action!");
@@ -212,7 +575,7 @@ void loop()
       if (deserializeJson(myDoc, respBuff) == DeserializationError::Ok)
       {
         String erg = myDoc["result"];
-        if(erg == "OK")
+        if (erg == "OK")
         {
           Serial.println("Transmit Success");
         }
@@ -224,11 +587,16 @@ void loop()
     }
   }
 
+  if (millis() - updateSensorMillis > (1000))
+  {
+    updateSensorMillis = millis();
+    updateSensorData();
+  }
 
   switch (cActionState)
   {
   case ACT_IDLE:
-    if(pendingAction)
+    if (pendingAction)
       cActionState = ACT_PREPARED;
     break;
 
@@ -241,47 +609,90 @@ void loop()
     break;
 
   case ACT_STARTING:
-    if(millis() - actionMillis > 1000)
+    if (millis() - actionMillis > 1000)
     {
       sprintf(reqBuff, "mac=%s&action=%d&value=%d", deviceID, actionID, 1);
       requestServer(currentClient, "http://api.graviplant-online.de", "/steuerbox/v1/actionLog/", reqBuff, respBuff, 2000);
       cActionState = ACT_RUNNING;
       actionMillis = millis();
-    }    
+    }
     break;
 
   case ACT_RUNNING:
-    if(millis() - actionMillis > action*1000)
+    if (millis() - actionMillis > action * 1000)
     {
       sprintf(reqBuff, "mac=%s&action=%d&value=%d", deviceID, actionID, 1);
       requestServer(currentClient, "http://api.graviplant-online.de", "/steuerbox/v1/actionLog/", reqBuff, respBuff, 2000);
       cActionState = ACT_STOPPING;
       actionMillis = millis();
-    }    
+    }
     break;
 
   case ACT_STOPPING:
-    if(millis() - actionMillis > 1000)
+    if (millis() - actionMillis > 1000)
     {
       sprintf(reqBuff, "mac=%s&action=%d&value=%d", deviceID, actionID, 0);
       requestServer(currentClient, "http://api.graviplant-online.de", "/steuerbox/v1/actionLog/", reqBuff, respBuff, 2000);
       cActionState = ACT_IDLE;
       actionMillis = millis();
       pendingAction = false;
-    }    
+    }
     break;
-  
+
   default:
     cActionState = ACT_IDLE;
     break;
   }
 
-  Ethernet.maintain();
+  // Ethernet.maintain();
+  dnsServer.processNextRequest();
+  webServer.handleClient();
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y)
+void updateSensorData()
 {
-  return x + y;
+
+  int32_t ain0, ain1, ain2, ain3;
+
+  ain0 = readPinCal(PIN_ADC0);
+  ain1 = readPinCal(PIN_ADC1);
+  ain2 = readPinCal(PIN_ADC2);
+  ain3 = readPinCal(PIN_ADC3);
+
+  uint8_t sout0, sout1;
+
+  sout0 = 0;
+  sout1 = 0;
+
+  sprintf(sensorDataBuff, R"EOF({
+"hwID":"%s",
+"serverstate":"%s",
+"wifissid":"%s",
+"wifiip":"%s",
+"wifistat_x":"%d",
+"ethip":"%s",
+"ethstate_y":"%d",
+"AIN0_mV":%d,
+"AIN1_mV":%d,
+"AIN2_mV":%d,
+"AIN3_mV":%d,
+"OUT0_s":%d,
+"OUT1_s":%d,
+"fwvers":"%s"
+})EOF",
+          deviceID,
+          serverStateStr,
+          WiFi.SSID().c_str(),
+          WiFi.localIP().toString().c_str(),
+          WiFi.status(),
+          Ethernet.localIP().toString().c_str(),
+          Ethernet.linkStatus(),
+          ain0,
+          ain1,
+          ain2,
+          ain3,
+          sout0,
+          sout1,
+          FW_VERS);
 }
---
+++ src/website.h
@@ -0,0 +1,176 @@
+#ifndef WEBPAGE_H
+#define WEBPAGE_H
+
+const char *webpageHeader1 = R"EOF(
+<!DOCTYPE html><html lang="en-US">
+<head><title>
+)EOF";
+
+const char *webpageHeader2 = R"EOF(
+</title>
+<meta name=viewport content="width=device-width,initial-scale=1">
+<style>
+article { background: #f2f2f2; padding: 1.3em; }
+body { color: #333; font-family: Century Gothic, sans-serif; font-size: 18px; line-height: 24px; margin: 0; padding: 0; }
+div { padding: 0.5em; }
+h1 { margin: 0.5em 0 0 0; padding: 0.5em; }
+h2 { background-color:red;}
+input { width: 100%; padding: 9px 10px; margin: 8px 0; box-sizing: border-box; border-radius: 0; border: 1px solid #555555; border-radius: 10px; }
+label { color: #333; display: block; font-style: italic; font-weight: bold; }
+nav { background: #0066ff; color: #fff; display: block; font-size: 1.3em; padding: 1em; }
+nav b { display: block; font-size: 1.5em; margin-bottom: 0.5em; } 
+textarea { width: 100%; }
+td {text-align: left;}
+th {text-align: left;min-width:150px;}
+</style>
+<script>
+var getJSON = function(url, callback) {
+var xhr = new XMLHttpRequest();
+xhr.open('GET', url, true);
+xhr.responseType = 'json';
+xhr.onload = function() {
+var status = xhr.status;
+if (status === 200) {
+callback(null, xhr.response);
+} else {
+callback(status, xhr.response);
+}
+};
+xhr.send();
+};
+</script>
+<meta charset="UTF-8">
+</head>
+<body>
+)EOF";
+
+const char *webpageDataTable = R"EOF(
+<table>
+<tr>
+<th>Allgemein</th>
+<th>Info</th>
+</tr>
+<tr>
+<td>Firmware</td>
+<td id="fwvers">-</td>
+</tr>
+<tr>
+<td>Server</td>
+<td id="serverstate">-</td>
+</tr>
+</table>
+
+<table>
+<tr>
+<th>WiFi</th>
+<th>Status</th>
+</tr>
+<tr>
+<td>SSID</td>
+<td id="wifissid">-</td>
+</tr>
+<tr>
+<td>Status</td>
+<td id="wifistat_x">-</td>
+</tr>
+<tr>
+<td>IP Adresse</td>
+<td id="wifiip">-</td>
+</tr>
+</table>
+
+<table>
+<tr>
+<th>Ethernet</th>
+<th>Status</th>
+</tr>
+<tr>
+<td>IP</td>
+<td id="ethip">-</td>
+</tr>
+<tr>
+<td>Status</td>
+<td id="ethstate_y">-</td>
+</tr>
+</table>
+
+<table>
+<tr>
+<th>Analog</th>
+<th>Status</th>
+</tr>
+<tr>
+<td>AIN0</td>
+<td id="AIN0_mV">-</td>
+</tr>
+<tr>
+<td>AIN1</td>
+<td id="AIN1_mV">-</td>
+</tr>
+<tr>
+<td>AIN2</td>
+<td id="AIN2_mV">-</td>
+</tr>
+<tr>
+<td>AIN3</td>
+<td id="AIN3_mV">-</td>
+</tr>
+</table>
+
+<table>
+<tr>
+<th>Ausgang</th>
+<th>Status</th>
+</tr>
+<tr>
+<td>OUT0</td>
+<td id="OUT0_s">-</td>
+</tr>
+<tr>
+<td>OUT1</td>
+<td id="OUT1_s">-</td>
+</tr>
+</table>
+)EOF";
+
+const char *webpageTableUpdateScript = R"EOF(
+<script>
+function retrieveData() {
+getJSON("/data", function(err, data) {
+if(err != null){
+console.log(err)
+}else{
+for (var key in data) {
+if (data.hasOwnProperty(key)) {
+if(document.getElementById(key) != null){
+document.getElementById(key).innerHTML = data[key];
+if(key.includes("_mA"))
+document.getElementById(key).innerHTML = (data[key]/1000.0).toFixed(3) + " A";
+if(key.includes("_mV"))
+document.getElementById(key).innerHTML = (data[key]/1000.0).toFixed(3) + " V";
+if(key.includes("_p"))
+document.getElementById(key).innerHTML = data[key] + " %";
+if(key.includes("_s"))
+document.getElementById(key).innerHTML = ["AUS", "AN"][data[key]];
+if(key.includes("_v"))
+document.getElementById(key).innerHTML = ["LOW", "HIGH"][data[key]];
+if(key.includes("_d"))
+document.getElementById(key).innerHTML = (data[key]/10.0).toFixed(1) + " °C";
+if(key.includes("_r"))
+document.getElementById(key).innerHTML = (data[key]/10.0).toFixed(1) + " %rH";
+}
+}
+}
+}
+});
+}
+var interval = setInterval(retrieveData, 1000);
+</script>
+)EOF";
+
+const char *webpageFooter = R"EOF(
+</body>
+</html>
+)EOF";
+
+#endif
